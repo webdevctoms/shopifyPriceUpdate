b41f2e6ab66108379b90f764a810e81ad8ffc290
Michael Peterson <WebDev@ctoms.ca>
2019-07-12 14:18:15 -0600

    setup initial db connection

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.PORT = process.env.PORT || 8000;
+exports.CKEY = process.env.CKEY;
+exports.URLUS = process.env.URLUS;
+exports.USERK = process.env.USERK;
+exports.USERP = process.env.USERP;
+exports.DATABASE_URL = process.env.DATABASE_URL;
---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const {PORT, DATABASE_URL } = require('./config');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const app = express();
+app.use(jsonParser);
+let server;
+
+function runServer( databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl,{ useNewUrlParser: true },err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer(){
+	return new Promise((resolve,reject) => {
+		console.log("closing server");
+		server.close(err => {
+			if(err){
+				return reject(err);
+			}
+			resolve();
+		});
+	});
+}
+
+if (require.main === module){
+	runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
