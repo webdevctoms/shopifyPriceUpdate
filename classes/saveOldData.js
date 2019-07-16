69c86d58ed51c705c86084fb5468b6a1da650aa5
Michael Peterson <WebDev@ctoms.ca>
2019-07-16 12:59:42 -0600

    adjusted save method to save all data

--- classes/saveOldData.js
+++ classes/saveOldData.js
@@ -24,17 +24,33 @@ OldData.prototype.getVariantData = function(variants) {
 OldData.prototype.saveData = function(productIndex) {
 	//save one product
 	let promise = new Promise((resolve,reject) => {
-		console.log("save product data: ",productIndex,this.productData.length);
-		let variantData = this.getVariantData(this.productData[productIndex].variants);
-		return this.priceModel.create({
-			product_id:this.productData[productIndex].id,
-			product_title: this.productData[productIndex].title,
-			variant_data:variantData
-		})
+		console.log("save product data: ",productIndex,this.productData.length,this.productData[productIndex].title);
+		if(productIndex !== this.productData.length){
+			let variantData = this.getVariantData(this.productData[productIndex].variants);
+			return this.priceModel.create({
+				product_id:this.productData[productIndex].id,
+				product_title: this.productData[productIndex].title,
+				variant_data:variantData
+			})
 
-		.then(data => {
-			resolve(data);
-		})
+			.then(data => {
+				resolve(this.saveData(productIndex + 1));
+			})
+
+			.catch(err => {
+				console.log("Error saving products: ",this.productData[productIndex].title,err);
+				if(err.errmsg.includes("E11000")){
+					resolve(this.saveData(productIndex + 1));
+				}
+				else{
+					resolve(err);
+				}		
+			})
+		}
+		else{
+			resolve("Done");
+		}
+		
 	});
 
 	return promise;
